5b9de647f401f92421c6d1e5cd286a1d3dd6861c
Yohan <yohancaro7@gmail.com>
2019-06-15 09:54:42 -0500

    Clean

--- src/co/edu/uptc/exceptions/ComExceptions.java
+++ src/co/edu/uptc/exceptions/ComExceptions.java
@@ -6,17 +6,4 @@ public class ComExceptions extends Exception {
 		super(message);
 	}
 	
-	/*
-	 * Crear una clase persona con id, nombre, apellido, numro y tipo de documento que mediante el controllador 
-	 * invocandolo dentro de un main
-	 * pueda persistir un listado utilizando la clase genericDAO y enum y tipo a almacenar
-	 * como salida 
-	 * Contro listado de personas
-	 * Modelo: unica persona
-	 * HashMap por cada enum un dao
-	 * Metodo persirtir que requiere el enum y by el listado de personas
-	 * ----------------------------------------------------------------------------------------------------------
-	 * Reportes
-	 * JasperReport->Tibco Studio
-	 */
 }
